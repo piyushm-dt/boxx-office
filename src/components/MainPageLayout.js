69c29ac835fa3c645c2d2b3164beca21b4fbe561
Pieter33 <piyushmanohar640@gmail.com>
2020-11-19 19:17:06 +0530

    show results and refactoring

--- src/components/MainPageLayout.js
+++ src/components/MainPageLayout.js
@@ -5,7 +5,7 @@ import Title from './Title';
 function MainPageLayout( { children }) {
     return (
     <div>
-        <Title title="Box Office" subtitle="Are you looking for movie or an actor ?"/>
+    <Title title="Box Office" subtitle="Are you looking for movie or an actor ?"/>
     <Navs />
     {children}
     </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 import MainPageLayout from '../components/MainPageLayout'
+import {apiGet} from '../misc/config';
 
 function Home() {
+
+    const [input, setInput] = useState('');
+    const [results, setResults] = useState(null);
+
+
+    const onInputChange = (ev) => {
+        setInput(ev.target.value);
+    };
+
+    const onSearch = () => {
+        apiGet(`/search/shows?q=${input}`).then(res => {
+            setResults(res);
+        });
+    };
+
+    const onKeyDown = (ev) => {
+        if(ev.keyCode === 13)
+            onSearch();
+    };
+
+    const renderResult = () => {
+        if(results && results.length === 0) {
+            return <div>
+                No results !
+            </div>
+        }
+        if(results && results.length > 0){
+            return (
+                <div>
+                    {
+                        results.map( item => (
+                            <div key={item.show.id} >{item.show.name}</div>
+                        ))
+                    }
+                </div>
+            );
+        }
+        return null;
+    }
+
     return (
         <MainPageLayout>
-            This is home
+            <input type="text" onChange={onInputChange} onKeyDown={onKeyDown} value={input} />
+            <button type="button" onClick={onSearch} >Search</button>
+            { renderResult() }
         </MainPageLayout>
     )
 }
