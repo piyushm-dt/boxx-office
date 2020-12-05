ffe7bb9d0022ad7b41640d0092626252ce7695a5
piyush manohar <piyushmanohar640@gmail.com>
2020-12-05 12:46:59 +0530

    created two more hooks

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import Star from './pages/Star';
+import Star from './pages/Starred';
 import Show from './pages/Show';
 
 function App() {
